cd386024ceeefa918de8e8fa26819598f56a3a25
sanr4-15 <sanr4_15@uni.worc.ac.uk>
2017-10-16 14:56:02 +0100

    initial

---
+++ www/js/index.js
@@ -0,0 +1,44 @@
+var paused_count =0;
+var resumed_count = 0;
+var launched_count = 0;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+		
+	
+function updateDisplay() {
+	$("#launched").text("Application launched: " + launched_count);
+	$("#resumed").text("Application paused: " + paused_count);
+	$("#paused").text("Application resumed: " + resumed_count);
+}
+
+
+// device APIs are available
+//
+    function onDeviceReady() {
+	
+	document.addEventListener("resume", onResume, false);
+	document.addEventListener("pause", onPause, false);
+	
+	launched_count++;
+	updateDisplay();
+	    
+	alert("device ready");
+    }
+
+
+    function onPause() {
+	
+	paused_count++;
+	updateDisplay();
+	    
+	alert("pause");
+    }
+	
+
+    function onResume() {
+		
+	resumed_count++;
+	updateDisplay();
+	    
+	alert("resume");
+    }
